8f436ec4aeda295a8848b49c562a593a716d96fa
Donatas Pocius <pocius.coding@gmail.com>
2022-08-03 18:13:55 +0300

    save

--- script.js
+++ script.js
@@ -4,42 +4,58 @@ let btns = document.querySelectorAll(".btn");
 let output = document.querySelector(".output");
 let n = 6;
 
+let triangle = function () {
+  let triangleOut = "";
+  for (let i = 1; i <= n; i++) {
+    for (let k = 0; k < i; k++) {
+      triangleOut += "*";
+    }
+    triangleOut += "<br>";
+  }
+  return triangleOut;
+};
+
+let pyramid = function () {
+  let pyramidOut = "";
+  for (let i = 1; i <= n; i++) {
+    for (let k = 1; k <= n - i; k++) {
+      pyramidOut += "&nbsp;";
+    }
+    for (let j = 0; j < i; j++) {
+      pyramidOut += "&nbsp;*&nbsp;";
+    }
+    pyramidOut += "<br>";
+  }
+  return pyramidOut;
+};
+
+let revPyramid = function () {
+  let revPyramidOut = "";
+  for (let i = 1; i <= n; i++) {
+    for (let k = 1; k <= n - i; k++) {
+      revPyramidOut += "&nbsp;*&nbsp;";
+    }
+    for (let j = 0; j < i; j++) {
+      revPyramidOut += "&nbsp;";
+    }
+    revPyramidOut += "<br>";
+  }
+  return revPyramidOut;
+};
+
 btns.forEach((btn) =>
   btn.addEventListener("click", function (e) {
     switch (e.target.textContent) {
       case "triangle": {
-        let triangle = function () {
-          let triangleOut = "";
-          for (let i = 1; i <= n; i++) {
-            for (let k = 0; k < i; k++) {
-              triangleOut += "*";
-            }
-            triangleOut += "<br>";
-          }
-          return triangleOut;
-        };
         output.innerHTML = triangle();
-
         break;
       }
       case "pyramid": {
-        // let pyramid = function () {
-        //     let pyramidOut = '';
-        //     for (let i = 1; i <= n; i++) {
-        //         // adding spaces
-        //         for (let j = 0; j < n - i; j++) {
-        //         }
-        //         // adding stars
-        //         for () {}
-        //         pyramidOut += '<br>'
-        //     }
-        //     return pyramidOut
-        // }
-        // output.innerHTML = pyramid;
-        // break;
+        output.innerHTML = pyramid();
+        break;
       }
       case "reverse pyramid": {
-        console.log("hello");
+        output.innerHTML = revPyramid();
         break;
       }
       case "clear": {
